0b0c03a40e48e97fe36d9d1b5cd20ccc42596290
unknown <kanaramsaini980@gmail.com>
2023-02-13 11:18:34 +0530

    commit

---
+++ Indexscrip5.js
@@ -0,0 +1,14 @@
+function Method1(){
+    var quotes = {
+      "—Theodore Zeldin":
+        "A dream is what makes people love life even when it is painful",
+      "—Nicole Reed":
+        "Sometimes the bad things that happen in our lives put us directly on the path to the best things that will ever happen to us.",
+      "—Muhammad Ali":
+        "Don’t quit. Suffer now and live the rest of your life as a champion.",
+      "—Pele":
+        "The more difficult the victory, the greater the happiness in winning.",
+        "-Kanaram": "Mistakes are proof that you’re trying.",
+        "-Nelson Mandela": "The greatest glory in living lies not in never falling, but in rising every time we fall. "
+    };
+    var author=Object.keys[]
